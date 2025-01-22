d01486fec91ca242baab699402cb1b8b228c4fec
maria <marcavmer@alu.edu.gva.es>
2025-01-22 10:54:57 +0100

    add function App

---
+++ PrimerProyecto/00-hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats" alt="el avatar" />
+                <div>
+                    <strong>Maria Cavaller</strong>
+                    <span>@macamer</span>
+                </div>
+            </header>
+            
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
