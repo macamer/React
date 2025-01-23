bfca5ebca7b2c9435a92fb0d8580e39b20b1ddc8
maria <marcavmer@alu.edu.gva.es>
2025-01-23 16:37:08 +0100

    style className App

--- PrimerProyecto/00-hola-mundo/src/App.jsx
+++ PrimerProyecto/00-hola-mundo/src/App.jsx
@@ -4,11 +4,11 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
     return (
-        <> 
+        <section className="App"> 
             <TwitterFollowCard userName='macamer' name='Maria Cavaller' />
             <TwitterFollowCard userName='midudev' name='Miguel Ángel Durán' />
             <TwitterFollowCard userName='elonmusk' name='Elon Musk' />
             <TwitterFollowCard userName='pheralb' name='Pablo Hernández' />
-        </>
+        </section>
     )
 }
