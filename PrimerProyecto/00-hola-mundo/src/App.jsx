807c390c69b8093f5548cce995fe22396e6f0765
maria <marcavmer@alu.edu.gva.es>
2025-01-23 16:21:10 +0100

    create className

--- PrimerProyecto/00-hola-mundo/src/App.jsx
+++ PrimerProyecto/00-hola-mundo/src/App.jsx
@@ -2,17 +2,20 @@ import './App.css'
 export function App() {
     return (
         //<article style={{ display: 'flex', alignItems: 'center', color: '#fff' }}> --> si quisieramos poner etilos debe ser como si fuera un objeto (js)
-        <article>
-            <header>
-                <img src="https://unavatar.io/kikobeats" alt="el avatar" />
-                <div>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img 
+                    className='tw-followCard-avatar'
+                    src="https://unavatar.io/kikobeats" 
+                    alt="el avatar" />
+                <div className='tw-followCard-info'>
                     <strong>Maria Cavaller</strong>
-                    <span>@macamer</span>
+                    <span className='tw-followCard-infoUserName'>@macamer</span>
                 </div>
             </header>
             
             <aside>
-                <button>Seguir</button>
+                <button className='tw-followCard-button'>Seguir</button>
             </aside>
         </article>
     )
