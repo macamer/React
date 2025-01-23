35d57652e06cf2d34d8befe7ea5a95033142523b
maria <marcavmer@alu.edu.gva.es>
2025-01-23 19:19:44 +0100

    use objects

--- PrimerProyecto/00-hola-mundo/src/App.jsx
+++ PrimerProyecto/00-hola-mundo/src/App.jsx
@@ -2,18 +2,43 @@ import './App.css'
 import './TwitterFollowCard.jsx'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        userName: 'macamer',
+        name: 'Maria Cavaller',
+        isFollowing: false
+    },
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo Hernández',
+        isFollowing: false
+    },
+    {
+        userName: 'elonmusk',
+        name: 'Elon Musk',
+        isFollowing: true
+    }
+]
+
 export function App() {
     return (
-        <section className="App"> 
-            <TwitterFollowCard userName='macamer' initialIsFollowing={true}> 
-                Maria Cavaller Mercadal
-            </TwitterFollowCard>
-            <TwitterFollowCard userName= 'midudev'> 
-                Miguel Ángel Duran
-            </TwitterFollowCard>
-            <TwitterFollowCard userName= 'pheralb'> 
-                Pablo Hernández
-            </TwitterFollowCard>
+        <section className='App'>
+            {
+                users.map(({ userName, name, isFollowing }) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
         </section>
     )
 }
