196019a5c1532f6265c803a081edb8e14514686a
maria <marcavmer@alu.edu.gva.es>
2025-01-23 19:10:47 +0100

    create span Dejar de seguir

--- PrimerProyecto/00-hola-mundo/src/TwitterFollowCard.jsx
+++ PrimerProyecto/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -31,7 +31,8 @@ export function TwitterFollowCard ({ children, userName, initialIsFollowing}) {
             {/* esto es un comentario */}
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
