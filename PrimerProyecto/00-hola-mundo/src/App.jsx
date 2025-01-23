1b3baa7cd135bd5d7b3b05a472ed12a251126421
maria <marcavmer@alu.edu.gva.es>
2025-01-23 08:03:06 +0100

    the way to put styles on jsx

--- PrimerProyecto/00-hola-mundo/src/App.jsx
+++ PrimerProyecto/00-hola-mundo/src/App.jsx
@@ -1,5 +1,6 @@
 export function App() {
     return (
+        //<article style={{ display: 'flex', alignItems: 'center', color: '#fff' }}> --> si quisieramos poner etilos debe ser como si fuera un objeto (js)
         <article>
             <header>
                 <img src="https://unavatar.io/kikobeats" alt="el avatar" />
