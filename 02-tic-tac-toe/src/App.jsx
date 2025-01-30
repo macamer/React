e531487db33edb9fe338a0e8ecb9eef21d64a8ce
maria <marcavmer@alu.edu.gva.es>
2025-01-30 19:38:08 +0100

    can select board

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,40 +1,74 @@
 import { useState } from 'react'
 
 const TURNS = {
-  X: 'x',
-  O: 'o'
+  X: '✖️',
+  O: '⭕'
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
 
-  return(
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null es que no hay ganador y false empate
+
+  const updateBoard = (index) => {
+    //no actualizar la posición si ya hay algo
+    if (board[index]) return
+
+    const newBoard = [...board]  //se copia para no mutar el array original
+    newBoard[index] = turn //x u o
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
+  return (
     <main className="board">
       <h1>Tic tac toe</h1>
       <section className="game">
-         {
+        {
           board.map((_, index) => {
             return (
-              <Square 
+              <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard} //no queremos pasar la función porque queremos que ejecute cuando queremos 
+              //no cada vez que se renderiza
               >
                 {board[index]}
               </Square>
             )
           })
-         }
+        }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn == TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn == TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
-  ) 
+  )
 }
 
 export default App
